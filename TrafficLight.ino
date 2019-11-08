f3d2a80559923e15d77205513ab9593c3a27c34c
cookie-monster- <ilive4legos+github@gmail.com>
2019-11-08 12:57:20 -0600

    add comments

--- TrafficLight.ino
+++ TrafficLight.ino
@@ -1,4 +1,4 @@
-
+// define LED pins
 #define R1 3
 #define Y1 4
 #define G1 5
@@ -8,7 +8,7 @@
   
 // the setup routine runs once when you press reset:
 void setup() {                
-  // initialize the digital pin as an output.
+  // initialize the LED pins as outputs.
   pinMode(R1, OUTPUT);
   pinMode(G1, OUTPUT);  
   pinMode(Y1, OUTPUT);
@@ -17,21 +17,27 @@ void setup() {
   pinMode(Y2, OUTPUT);     
 }
 
+// control red green and yellow LEDs of first "traffic light"
 void rgy1(double r, double g, double y) {
     digitalWrite(R1, r);
     digitalWrite(G1, g);
     digitalWrite(Y1, y);
 }
+// control red green and yellow LEDs of second "traffic light"
 void rgy2(double r, double g, double y) {
     digitalWrite(R2, r);
     digitalWrite(G2, g);
     digitalWrite(Y2, y);
 }
 
+// initialize count and delay variables
 int count = 0;
 int delayTime = 0;
+
 // the loop routine runs over and over again forever:
 void loop() {
+  //this switch determines what part of the sequence of traffic lights we are in
+  //it then turns "on" the correct LEDs, and gives the main program an approriate delayTime
   switch (count) {
     case 0://red red
       rgy1(HIGH,LOW,LOW);
@@ -64,9 +70,11 @@ void loop() {
       delayTime = 1500;
       break;
   }
-  
+
+  //increase the counter, keeping it between 0-5
   count ++;
   count %= 6;
-  delay(delayTime);
 
+  //delay next cycle based on what part of cycle we are in (e.g. don't want both lights to be red for very long)
+  delay(delayTime);
 }
